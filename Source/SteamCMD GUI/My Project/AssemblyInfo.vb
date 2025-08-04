30932a8e4358b07d39a9aa348c95e5bba4472235
BK1233 <12680340+BK1233@users.noreply.github.com>
2025-08-04 02:03:39 +0000

    revert: Revert version number to 3.1.0.2

--- Source/SteamCMD GUI/My Project/AssemblyInfo.vb
+++ Source/SteamCMD GUI/My Project/AssemblyInfo.vb
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.0.0.0")> 
-<Assembly: AssemblyFileVersion("2.0.0.0")> 
+<Assembly: AssemblyVersion("3.1.0.2")> 
+<Assembly: AssemblyFileVersion("3.1.0.2")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")>
